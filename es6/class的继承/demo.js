04c53db4581a2f15c09a70b869ab11a3e150d65a
pineapple <longchao.zhang@b-and-qchina.com>
2019-04-24 20:08:56 +0800

    class extend

---
+++ es6/class的继承/demo.js
@@ -0,0 +1,19 @@
+class Color{
+  //静态方法也会被子类继承
+  static hello(){
+    console.log('hello world');
+  }
+}
+class YelloColor extends Color{
+  constructor(r,g,b){
+    super()//一定要有super
+    this.r = r
+    this.g = g
+    this.b = b
+  }
+}
+let yello = new YelloColor(255,0,0)
+YelloColor.hello()
+
+//判断一个类是否继承自另一个类
+console.log(Object.getPrototypeOf(YelloColor));
