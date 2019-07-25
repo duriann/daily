5672c0c8d51630f5adedcd472edf9e895ab1d7f2
pineapple <longchao.zhang@b-and-qchina.com>
2019-07-25 11:02:53 +0800

    add 3.5.3

--- functional-js/chapter3/index.js
+++ functional-js/chapter3/index.js
@@ -1,1 +1,27 @@
 let _ = require('underscore')
+
+
+//3.5.3 闭包的抽象
+function plucker(FIELD){
+  return function(obj){
+    return obj && obj[FIELD]
+  }
+}
+let best = {
+  title: 'functional js',
+  author: 'bl'
+}
+let getTitle = plucker('title')
+
+let title = getTitle(best)
+console.log('title: ', title);
+
+let books = [{title: 'bl'},{stars: 5},{title: 'hx'}]
+let titles = _.filter(books, (item,index,books)=>{
+  if(item['title']){
+    return item
+  } 
+})
+console.log('titles ',titles);
+let titles2 = _.filter(books,getTitle)
+console.log('titles2 ',titles2)
