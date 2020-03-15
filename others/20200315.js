457ab2b8794f9f9416f440beee487d66fcea3281
pineapple <89190006@qq.com>
2020-03-15 15:12:16 +0800

    'fix'

--- others/20200315.js
+++ others/20200315.js
@@ -19,13 +19,17 @@ let arr = [
   { id: 'Everyone', acl: [ 'READ_ACP', 'WRITE_ACP' ] } ]
  */
 
- let res = arr.reduce((prev,current)=>{
-  let exit = prev.filter(item=>item.id === current.id)
-  if(exit.length){
-    exit[0]['acl'] = [...(exit[0]['acl']),current['acl']]
-    return Array.from(new Set([...prev,...exit]))
-  }
-  current['acl'] = [current['acl']]
-  return Array.from(new Set([...prev,current]))
- },[])
+ function combine(arr,column){
+  return arr.reduce((prev,current)=>{
+    let exit = prev.filter(item=>item.id === current.id)
+    if(exit.length){
+      exit[0][column] = [...(exit[0][column]),current[column]]
+      return [...(prev.splice(prev.findIndex(p=>p.id === exit.id),1),prev),...exit]
+    }
+    current[column] = [current[column]]
+    return [...prev,current]
+   },[])
+ }
+
+ let res = combine(arr,'acl')
  console.log('res',res);
