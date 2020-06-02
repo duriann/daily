729a1df07935e4e2211018d805f9e4a29a41419d
pineapple <longchao.zhang@b-and-qchina.com>
2020-06-02 18:15:06 +0800

    add flat.js

---
+++ interview/fake-jsonp/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/hello',(req,res)=>{
+  console.log('req',req.params,req.query);
+  res.send(`${req.query.callback}({a:1})`);
+})
+app.listen(9999,()=>{
+  console.log('启动啦');
+})
