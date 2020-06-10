23c84d7bbda08c01c462fe0eb5445ace88621128
pineapple <longchao.zhang@b-and-qchina.com>
2020-06-10 17:58:11 +0800

    ..

--- interview/webpack/webpack.config.js
+++ interview/webpack/webpack.config.js
@@ -1,22 +1,52 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname,'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  devServer:{
+  devServer: {
     port: 9999,
     open: true,
-    hot: true
+    hot: true,
   },
-  plugins:[
+  module: {
+    rules: [
+      {
+        test: /\.(css|less)/,
+        loader: ['style-loader','css-loader','less-loader']
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              // limit表示如果图片大于5KB，就以路径形式展示，小于的话就用base64格式展示
+              limit: 5 * 1024,
+              // 打包输出目录
+              outputPath: 'images',
+              // 打包输出图片名称
+              name: '[name]-[hash:4].[ext]'
+          }
+          }
+        ]
+      },
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        loader: ['babel-loader']
+      }
+    ]
+  },
+  plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'template.html'
-    })
-  ]
-}
+      filename: "index.html",
+      template: "template.html",
+    }),
+  ],
+
+};
