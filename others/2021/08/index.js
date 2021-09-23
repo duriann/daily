21d478c10773c5399c4213a8e8f8a49db5c7eabc
pineapple <longchao.zhang@b-and-qchina.com>
2021-09-23 14:29:23 +0800

    feat: some more

---
+++ others/2021/08/index.js
@@ -0,0 +1,50 @@
+let arr = [
+  {
+    id: '01',
+    name: 'useradmin',
+    titlle: '用户管理',
+    children: [
+      {
+        id: '0101',
+        name: 'roaladmin',
+        titlle: '角色管理',
+        children: [
+          {
+            id:'010101',
+            name: 'delbtn',
+            title:'删除按钮'
+          },
+          {
+            id:'010102',
+            name: 'delbtn',
+            title:'删除按钮'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: '02',
+    name: 'aaa',
+    titlle: 'aaaa',
+    children: [
+      {
+        id: '0202',
+        name: 'bbb',
+        titlle: 'bbbb',
+        children: [
+          {
+            id:'020201',
+            name: 'ccc',
+            title:'cccc'
+          },
+          {
+            id:'020202',
+            name: 'ddd',
+            title:'dddd'
+          }
+        ]
+      }
+    ]
+  }
+]
